c4a53f6c6a8d650402a1394769681b6f2bf890c3
alemartinn <ale_xeg147@hotmail.com>
2022-08-19 20:14:39 -0300

    add UnderConstruction page

---
+++ src/pages/UnderConstruction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BasePage from '../layouts/BasePage';
+
+const UnderConstruction = () => {
+    return (  
+        <BasePage>
+            {/* Children */}
+        </BasePage>
+    );
+}
+
+export default UnderConstruction;
