cf46ef9dbb7e0aeb5f59372a5f87431cf0676cbb
alemartinn <ale_xeg147@hotmail.com>
2022-08-23 18:54:41 -0300

    Deleted some lines irrelevants

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,6 +1,5 @@
 import Button from '../components/Button'
 import '../styles/Hero.css'
-import Footer from '../components/Footer';
 import CityCarousel from '../components/DataCity';
 
 export default function Hero() {
