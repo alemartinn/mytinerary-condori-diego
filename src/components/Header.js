3004363b2bedbb59dd39db977bcc2e5aad7bbcdd
alemartinn <ale_xeg147@hotmail.com>
2022-08-24 01:32:42 -0300

    We still are working on Header.js Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link as LinkRouter } from 'react-router-dom';
+import '../styles/Header.css'
 
 const Header = () => {
     return (  
         <header>
-            <nav>
-                <LinkRouter to='/'> Home </LinkRouter>
-                <LinkRouter to='/cities'> Cities </LinkRouter>
-                <LinkRouter to='/newcities'> NewCities </LinkRouter>
+            <nav className='HeaderNav'>
+                <LinkRouter to='/'> HOME </LinkRouter>
+                <LinkRouter to='/cities'> CITIES </LinkRouter>
+                <LinkRouter to='/newcities'> NEW CITIES </LinkRouter>
             </nav>
-            <h2>Titulo</h2>
-            <button>
-                <i></i>
+            <h3 className='HeaderTitle'>
+                <img src='/logoMyTinerary.png' alt='logoMyTinerary'/>
+            </h3>
+            <button className='HeaderButton'>
+                <img src='https://png.pngitem.com/pimgs/s/130-1300400_user-hd-png-download.png' alt='logoIconUser'/>
             </button>
         </header>
     );
