2cf58f8d628849be6a81b56bdcf3b54f9f9aa509
alemartinn <ale_xeg147@hotmail.com>
2022-08-25 02:36:34 -0300

    Added error 404 page

---
+++ src/pages/Error404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error404 = () => {
+    return (  
+        
+        <div>
+            <h2>Error 404</h2>
+            <p>This web doesn't exists, is under construction or has been moved of place.</p>
+        </div>
+    );
+}
+ 
+export default Error404;
