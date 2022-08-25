c93cbf03a3ce7df628e2520777bcc53054d719d1
alemartinn <ale_xeg147@hotmail.com>
2022-08-25 05:25:49 -0300

    Language has been changed

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -2,18 +2,18 @@ import React, { useEffect } from 'react';
 import { Link as LinkRouter } from 'react-router-dom';
 import '../../styles/Header/NavBar.css'
 
-const NavBar = ({mostrarMenu, clickMostrarMenu}) => {
+const NavBar = ({showNavMenu, clickShowNavMenu}) => {
     
     useEffect(() => {
         let headerNav = document.getElementById('HeaderNav')
-        mostrarMenu ? headerNav.style.left='0%' : headerNav.style.left='-100%'
-    },[mostrarMenu])
+        showNavMenu ? headerNav.style.left='0%' : headerNav.style.left='-100%'
+    },[showNavMenu])
 
     return (  
         <nav className='HeaderNav' id='HeaderNav'>
-            <LinkRouter to='/' onClick={clickMostrarMenu}> HOME </LinkRouter>
-            <LinkRouter to='/cities' onClick={clickMostrarMenu}> CITIES </LinkRouter>
-            <LinkRouter to='/newcities' onClick={clickMostrarMenu}> NEW CITIES </LinkRouter>
+            <LinkRouter to='/' onClick={clickShowNavMenu}> HOME </LinkRouter>
+            <LinkRouter to='/cities' onClick={clickShowNavMenu}> CITIES </LinkRouter>
+            <LinkRouter to='/newcities' onClick={clickShowNavMenu}> NEW CITIES </LinkRouter>
         </nav>
     );
 }
