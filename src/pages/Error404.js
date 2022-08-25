43c39c5507cb323c06caf70f78a6e647fb8c3097
alemartinn <ale_xeg147@hotmail.com>
2022-08-25 02:59:52 -0300

    added className to div

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../styles/Error404.css'
 
 const Error404 = () => {
     return (  
         
-        <div>
+        <div className='ErrorContainer'>
             <h2>Error 404</h2>
             <p>This web doesn't exists, is under construction or has been moved of place.</p>
         </div>
