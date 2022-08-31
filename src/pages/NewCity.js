74ec9de27cd8c55fd8d0cc5e1ecafb4049b715c5
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-08-31 05:14:28 -0300

    Add component inputMod

--- src/pages/NewCity.js
+++ src/pages/NewCity.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import InputMod from '../components/InputMod';
+import '../styles/NewCity.css'
 
 export default function NewCity() {
+    const [values, setValues] = React.useState()
+
+    const formOdel = [
+        {name: 'City', type: 'text'},
+        {name: 'Country', type: 'text'},
+        {name: 'Photo', type: 'text'},
+        {name: 'Population', type: 'number'}
+    ]
+
+    const formRef = React.useRef()
+    const submit = event => {
+        event.preventDefault()
+        const form = new formOdel(formRef.current)
+        setValues(Object.fromEntries(form))
+    }
+
+    const viewForm = (input) => (
+        <fieldset className='NewCity-fieldset'>
+            <label>{input.name}: <InputMod type={input.type} name={input.name} /></label>
+        </fieldset>
+    )
+
     return (
-        <p>New Cities</p>
+        <div className='Input-container'>
+            <form className='NewCity-form' onSubmit={submit} ref={formRef}>
+                <h2 className='Title-form'>New City</h2>
+                <div className='Inputs-form'>{formOdel.map(viewForm)}</div>
+                <button className='Submit-form' type='submit'>Send</button>
+            </form>
+        </div>
     )
   }
   
