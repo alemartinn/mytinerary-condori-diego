4615582a9daee7991886277ec82a12f878b0c581
alemartinn <ale_xeg147@hotmail.com>
2022-08-31 00:56:01 -0300

    We changed prop.img to prop.photo

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -4,7 +4,7 @@ const CityCard = (props) => {
     return (
         <div className='cityCard' key={props.id}>
             <h3>{props.title}</h3>
-            <img src={props.img} alt={props.city} className='city-img'/>
+            <img src={props.photo} alt={props.city} className='city-img'/>
         </div>
     )
 }
