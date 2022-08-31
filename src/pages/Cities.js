74d68fa4aa556c1b83b60e29385a8581a7f373db
alemartinn <ale_xeg147@hotmail.com>
2022-08-30 23:45:22 -0300

    we implemented axios to get the data of the cities

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import { cities } from '../components/DataCity'
+import React, { useState, useEffect } from 'react'
+// import { cities } from '../components/DataCity'
 import CityCard from '../components/CityCard'
 import '../styles/Cities.css'
+import axios from 'axios'
 
 const Cities = () => {
     
+    const [dataCities, setCities] = useState([]);
+    
+    useEffect(()=>{
+        axios.get('http://localhost:4000/cities/')
+            .then(response=> setCities(response.data.response))
+            .catch(error => console.log(error))
+    },[])
+
     return (  
         <div className='citiesContainer'>
-            {cities.map(city =>{
-                return <CityCard id={city._id} title={city.city} img={city.img} />
-            })}
+            {dataCities.map((city, index) => <CityCard id={city._id} title={city.city} img={city.img} key={index} />)}
         </div>
     );
 }
