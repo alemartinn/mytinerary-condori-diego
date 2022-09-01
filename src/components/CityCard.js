b98fbf506e9e11106baa61ea2f18c0760c5593bb
alemartinn <ale_xeg147@hotmail.com>
2022-09-01 07:51:49 -0300

    Added dinamics links

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,10 +1,14 @@
-import '../styles/Cities.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Cities.css';
 
 const CityCard = (props) => {
     return (
         <div className='cityCard' key={props.id}>
             <h3>{props.title}</h3>
-            <img src={props.photo} alt={props.city} className='city-img'/>
+            <Link to={`/details/${props.id}`}>
+                <img src={props.photo} alt={props.city} className='city-img'/>
+            </Link>
         </div>
     )
 }
