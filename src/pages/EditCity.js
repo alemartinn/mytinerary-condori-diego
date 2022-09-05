5d37b3d3cf7402ed7c8bffbfac35835ac610eae7
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-04 21:51:43 -0300

    autocomplete attempt

--- src/pages/EditCity.js
+++ src/pages/EditCity.js
@@ -6,7 +6,10 @@ import '../styles/EditCity.css';
 export default function EditCity() {
 
     const [dataCities, setCities] = useState([]);
+    const [cityToEdit, setIdCityToEdit] = useState("")
     const formOdel = [
+        {name: 'city', type: 'text'},
+        {name: 'country', type: 'text'},
         {name: 'photo', type: 'text'},
         {name: 'population', type: 'number', min: 1000, max:1000000000},
         {name: 'fundation', type: 'number', min: 1000, max: 2022}
@@ -17,25 +20,38 @@ export default function EditCity() {
             .then(response=> setCities(response.data.response))
             .catch(error => console.log(error))
     },[])
-    
-    const options = (city, index) => (
-        <option className='options' value={city.city} key={index}>{city.city}</option>
+    const options = (city) => (
+        <option className='options' value={city.city} key={city._id} data_key={city._id}>{city.city}</option>
     )
     
     const viewForm = (input, i) => (
 
         <div className='form__group field' key={i}>
-            <InputMod clase='form__field' id={input.name} type={input.type} name={input.name} min={`${input.type === 'number' && input.min}`} max={`${input.type === 'number' && input.max}`}/>
-            <label className='form__label' htmlFor={input.name}>{input.name}</label>  
+            <InputMod clase='form__field' id={input.name} type={input.type} name={input.name} min={`${input.type === 'number' && input.min}`} max={`${input.type === 'number' && input.max}`}></InputMod>
+            <label className='form__label' htmlFor={input.name}>{input.name}</label>
         </div>
     )
+    
+    const editingCity = async (e) => {
+        console.log(e.target.value)
+        console.log(e.target.options[e.target.options.selectedIndex].getAttribute("data_key"))
+        await axios.get('http://localhost:4000/cities/' + e.target.options[e.target.options.selectedIndex].getAttribute("data_key"))
+        .then(response => setIdCityToEdit(response.data.response))
+        .catch(error => console.log(error))
+    }
 
+    useEffect(() => {
+        console.log(cityToEdit)
+    } , [cityToEdit])
+
+
+    
   return (
     <div className='Edit-container'>
         <h2 className='Edit-title'>Choose a City</h2>
         <div className='select-container'>
-            <select className='Select' name='cities'>
-            <option defaultValue>--Select City--</option>
+            <select className='Select' name='cities'onChange={(e) => editingCity(e)} >
+            <option>--Select City--</option>
             {dataCities.map(options)}
             </select>
         </div>
