23d3b5d9dfb55bdc22c2638d62cb34a4525f8202
alemartinn <ale_xeg147@hotmail.com>
2022-09-05 19:04:44 -0300

    removing innecesaries lines

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -1,30 +1,19 @@
-import React, { useState, /*useEffect*/ } from 'react';
+import React, { useState } from 'react';
 import CityCard from '../components/CityCard';
 import '../styles/Cities.css';
-// import axios from 'axios';
 import { useGetAllCitiesQuery } from '../features/citiesAPI';
 
 const Cities = () => {
     
-    // const [dataCities, setDataCities] = useState([]);
     const [inputCity, setInputCity] = useState('');
 
     const handleInput = (e) => {
         setInputCity(e.target.value);
     }
-    
-    // useEffect(()=>{
-    //     axios.get("http://localhost:4000/cities/?city="+inputCity)
-    //         .then(response=> setDataCities(response.data.response))
-    //         .catch(error => console.log(error));
-    // },[inputCity]);
 
     const { 
-        data: cities, 
-        // error, 
-        // isLoading, 
-        isSuccess, 
-        // isFailed 
+        data: cities,
+        isSuccess
     } = useGetAllCitiesQuery(inputCity);
     
     return (  
