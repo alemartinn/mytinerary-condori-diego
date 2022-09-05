9c016cf99a644d32124e1e4f9c2554830a7edf1b
alemartinn <ale_xeg147@hotmail.com>
2022-09-05 18:48:24 -0300

    we implemented redux and new hook useGetOneCityQuery

--- src/components/DetailsCard.js
+++ src/components/DetailsCard.js
@@ -1,30 +1,39 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { useGetOneCityQuery } from '../features/citiesAPI';
 
 const DetailsCard = () => {
 
   let {id} = useParams();
 
-  const [dataCity, setCity] = useState();
+  // const [dataCity, setCity] = useState();
 
-  useEffect(()=>{
-      axios.get('http://localhost:4000/cities/?id='+id)
-          .then(response=> setCity(response.data.response))
-          .catch(error => console.log(error))
-  },[id])
+  // useEffect(()=>{
+  //     axios.get('http://localhost:4000/cities/?id='+id)
+  //         .then(response=> setCity(response.data.response))
+  //         .catch(error => console.log(error))
+  // },[id])
 
+
+  const { 
+    data: dataCity, 
+    // error, 
+    // isLoading, 
+    isSuccess, 
+    // isFailed 
+} = useGetOneCityQuery(id);
 
   return ( 
     <>
-      {dataCity 
+      { isSuccess 
       ? 
       <div className='detailsCardContainer'>
-          <h2>City: {dataCity[0].city}</h2>
-          <p>Country: {dataCity[0].country}</p>
-          <img src={dataCity[0].photo} alt={dataCity[0].city}></img>
-          <p>Population: {dataCity[0].population}</p>
-          <p>Fundation: {new Date(dataCity[0].fundation).getFullYear()}</p>
+          <h2>City: {dataCity.response.city}</h2>
+          <p>Country: {dataCity.response.country}</p>
+          <img src={dataCity.response.photo} alt={dataCity.response.city}></img>
+          <p>Population: {dataCity.response.population}</p>
+          <p>Fundation: {new Date(dataCity.response.fundation).getFullYear()}</p>
       </div>
       : 
       <div>
