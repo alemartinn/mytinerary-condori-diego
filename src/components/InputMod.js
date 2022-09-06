a3867a1dc8e5cb114cc7085ba558ed6cb8a97015
alemartinn <ale_xeg147@hotmail.com>
2022-09-06 20:07:06 -0300

    header with fixed

--- src/components/InputMod.js
+++ src/components/InputMod.js
@@ -3,15 +3,6 @@ import React, {forwardRef} from "react";
 const InputMod = forwardRef((props, ref) => {
         
     return(
-        /*<input 
-        className={props.clase} 
-        name={props.name} 
-        type={props.type} 
-        required 
-        ref={ref}
-        min={`${props.type === 'number' && props.min}`} 
-        max={`${props.type === 'number' && props.max}`} 
-        />*/
         <input {...props} ref={ref}/>
     )
 });
