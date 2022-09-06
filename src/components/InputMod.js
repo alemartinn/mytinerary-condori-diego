db3232feea12cca48b8e146bae3093ce532642de
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-06 20:06:07 -0300

    Optimize component

--- src/components/InputMod.js
+++ src/components/InputMod.js
@@ -3,15 +3,7 @@ import React from "react";
 const InputMod = React.forwardRef((props, ref) => {
         
     return(
-        <input 
-        className={props.clase} 
-        name={props.name} 
-        type={props.type} 
-        required 
-        ref={ref}
-        min={`${props.type === 'number' && props.min}`} 
-        max={`${props.type === 'number' && props.max}`} 
-        />
+        <input {...props} ref={ref}/>
     )
 });
 export default InputMod
