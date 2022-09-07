64751494822fd9ce06d5e14a74f6d3dff79e66ff
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-07 15:54:53 -0300

    Change placeholder

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -24,7 +24,7 @@ const Cities = () => {
                         <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
                     </svg>
                 </button>
-                <input className="input" placeholder="Type your text" required="" type="text" value={inputCity} onChange={(e)=> handleInput(e)}/>
+                <input className="input" placeholder="Search a city" required="" type="text" value={inputCity} onChange={(e)=> handleInput(e)}/>
                 <button className="reset" onClick={()=>inputCity("")}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="3">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
