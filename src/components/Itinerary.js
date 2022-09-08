ae28105e3c9ab5a7a56cc7b8970379b1a5a22dd4
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-08 07:37:24 -0300

    Update component itinerary

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -5,17 +5,15 @@ export default function Itinerary(props) {
     const itinerary = props.itinerary
   return (
     <div className='Itinerary-container'>
-        <h2>{itinerary.name}</h2>
+        <h2 className='Itinerary-Title'>{itinerary.name}</h2>
         <div className='Itinerary-userContainer'>
-            <img src={itinerary.user.photo}/>
+            <img className='Itinerary-userPhoto' src={itinerary.user.photo}/>
             <div className='Itinerary-userInfo'>
                 <p>{itinerary.user.name} {itinerary.user.lastName}</p>
             </div>
         </div>
         <div className='Itinerary-infoContainer'>
-            <p>{itinerary.city.city}</p>
-            <p>{itinerary.price}</p>
-            <p>{itinerary.duration}</p>
+            <p>Price: ${itinerary.price}</p>
         </div>
     </div>
   )
