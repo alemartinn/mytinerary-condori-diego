6f2b64b9090ada655bdf3e7e6534a27db0806699
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-08 09:30:08 -0300

    add comments api

--- src/store.js
+++ src/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import citiesAPI from "./features/citiesAPI";
 import citiesSlices from "./features/citiesSlices";
+import commentsAPI from "./features/commentAPI";
 import itinerariesAPI from "./features/itinerariesAPI";
 
 export default configureStore ({
     reducer: {
         cities: citiesSlices,
         [citiesAPI.reducerPath] : citiesAPI.reducer,
-        [itinerariesAPI.reducerPath] : itinerariesAPI.reducer
+        [itinerariesAPI.reducerPath] : itinerariesAPI.reducer,
+        [commentsAPI.reducerPath] : commentsAPI.reducer
     },
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(citiesAPI.middleware).concat(itinerariesAPI.middleware)
+    getDefaultMiddleware().concat(citiesAPI.middleware).concat(itinerariesAPI.middleware).concat(commentsAPI.middleware)
 })
