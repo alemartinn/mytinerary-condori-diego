419e16d21a327c96b55c1621b635b6a6dadcd71a
alemartinn <ale_xeg147@hotmail.com>
2022-09-08 09:33:18 -0300

    added new reducers

--- src/store.js
+++ src/store.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import citiesAPI from "./features/citiesAPI";
 import citiesSlices from "./features/citiesSlices";
 import itinerariesAPI from "./features/itinerariesAPI";
+import activitiesAPI from "./features/activitiesAPI";
 
 export default configureStore ({
     reducer: {
         cities: citiesSlices,
         [citiesAPI.reducerPath] : citiesAPI.reducer,
-        [itinerariesAPI.reducerPath] : itinerariesAPI.reducer
+        [itinerariesAPI.reducerPath] : itinerariesAPI.reducer,
+        [activitiesAPI.reducerPath] : activitiesAPI.reducer
     },
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(citiesAPI.middleware).concat(itinerariesAPI.middleware)
+    getDefaultMiddleware().concat(citiesAPI.middleware).concat(itinerariesAPI.middleware).concat(activitiesAPI.middleware)
 })
