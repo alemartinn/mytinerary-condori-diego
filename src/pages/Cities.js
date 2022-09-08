a0a14d064d2d77a7f2e6fada127fe61c9feb9f37
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-08 09:29:52 -0300

    delete import component itineraries

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import CityCard from '../components/CityCard';
 import '../styles/Cities.css';
 import { useGetAllCitiesQuery } from '../features/citiesAPI';
-import Itineraries from '../components/Itineraries';
+
 
 const Cities = () => {
     
