7a4d6d65fe6ad519a85b056d93c90659ed893c8f
alemartinn <ale_xeg147@hotmail.com>
2022-09-13 01:41:10 -0300

    Created new page to sign up

---
+++ src/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignUpGoogle from '../components/SignUpGoogle';
+
+const SignUp = () => {
+    return (  
+    <div>
+        <SignUpGoogle/>
+        <form action=''>
+            <input type="text"/>
+        </form>
+    </div>);
+}
+ 
+export default SignUp;
