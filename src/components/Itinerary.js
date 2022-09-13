178518f5ed63dcdf63a8d79397c7d3dcf711fb09
alemartinn <ale_xeg147@hotmail.com>
2022-09-13 16:03:53 -0300

    editing itineraries style

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import Comments from './Comments';
 import { useGetCommentsQuery } from '../features/commentAPI';
+import Activities from './Activities';
 import '../styles/Itinerary.css';
-import Activities from './Activities'
 
 export default function Itinerary(props) {
-    const itinerary = props.itinerary
-    const {data} = useGetCommentsQuery(itinerary._id)
-    if(data){
-        console.log(data)
-    }
+    const itinerary = props.itinerary;
+    const {data} = useGetCommentsQuery(itinerary._id);
+
   return (
     <div className='Itinerary-container'>
-        <h2 className='Itinerary-Title'>{itinerary.name}</h2>
-        <div className='Itinerary-userContainer'>
-            <img className='Itinerary-userPhoto' src={itinerary.user.photo} alt='User'/>
+        <div className='Itinerary-header'>
             <div className='Itinerary-userInfo'>
+                <img className='Itinerary-userPhoto' src={itinerary.user.photo} alt='User'/>
                 <p>{itinerary.user.name} {itinerary.user.lastName}</p>
             </div>
+            <div className='Itinerary-userContainer'>
+                <h2 className='Itinerary-Title'>{itinerary.name}</h2>
+            </div>
         </div>
-        <div className='Itinerary-infoContainer'>
-            <p>Price: ${itinerary.price}</p>
+        <div className='Itinerary-main'>
+                <p className='Itinerary-price'>Price: ${itinerary.price}</p>
             <Activities id={itinerary._id}/>
         </div>
         {data && data.response 
         ? 
         data.response.map(comment => <Comments key={comment._id} comment={comment} />)
         : 
-        <h3 className='Itinerary-NotComment'>There are no comments</h3>}
+        null
+        }
     </div>
   )
 }
