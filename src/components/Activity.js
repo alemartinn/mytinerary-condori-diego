7fd2516ca5824681a66878817ebaaf5cf9056440
alemartinn <ale_xeg147@hotmail.com>
2022-09-14 01:46:38 -0300

    we modified the structure of activities

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -3,11 +3,11 @@ import '../styles/Activities.css';
 
 const Activity = ({activt}) => {
     return (  
-        <div className='activityContainer'>
-            <p>Activity: {activt.name}</p>
-            <span>Img:
+        <div className='cardActivity'>
+            <p className='cardActivity-p'>{activt.name}</p>
+            <div className='cardActivity-img'>
                 <img src={activt.photo} alt={activt.name}/>
-            </span>
+            </div>
         </div>
     );
 }
