2dda4f15be5881aa77f2757c091753a79bafd8d6
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-15 19:47:45 -0300

    fixing padding and width to form

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import SignInGoogle from '../components/SignInGoogle';
+import FormSignIn from '../components/FormSignIn';
+import { Navigate } from 'react-router-dom';
 
-const SignUp = () => {
+const SignIn = () => {
+
+    let userLocal = JSON.parse(localStorage.getItem("client"));
+    let loggedInLocal;
+    if (userLocal){
+        loggedInLocal = userLocal.loggedIn;
+    }
+
     return (  
-    <div>
-        <SignInGoogle/>
-        <form action=''>
-            <input type="text"/>
-        </form>
-    </div>);
+        <>
+            {
+                loggedInLocal 
+                ?
+                <Navigate to="/" replace="true"/>
+                :
+                <div className='SignUp-container'>
+                    <FormSignIn/>
+                    <SignInGoogle/>
+                </div>
+            }
+        </>
+    )
 }
- 
-export default SignUp;
+
+export default SignIn;
