c6dc54a73f21fa9edbe26718fe79e7022574d441
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-15 02:32:48 -0300

    add authAPI

--- src/store.js
+++ src/store.js
@@ -3,13 +3,15 @@ import citiesAPI from "./features/citiesAPI";
 import commentsAPI from "./features/commentAPI";
 import itinerariesAPI from "./features/itinerariesAPI";
 import activitiesAPI from "./features/activitiesAPI";
+import { authAPI } from "./features/authAPi";
 
 export default configureStore ({
     reducer: {
         [citiesAPI.reducerPath] : citiesAPI.reducer,
         [itinerariesAPI.reducerPath] : itinerariesAPI.reducer,
         [commentsAPI.reducerPath] : commentsAPI.reducer,
-        [activitiesAPI.reducerPath] : activitiesAPI.reducer
+        [activitiesAPI.reducerPath] : activitiesAPI.reducer,
+        [authAPI.reducerPath] : authAPI.reducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(citiesAPI.middleware).concat(itinerariesAPI.middleware).concat(activitiesAPI.middleware).concat(commentsAPI.middleware)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(citiesAPI.middleware).concat(itinerariesAPI.middleware).concat(activitiesAPI.middleware).concat(commentsAPI.middleware).contact (authAPI.middleware)
 })
