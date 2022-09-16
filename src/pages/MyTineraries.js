9bc3c130e1ecd168d2697a0a0897ec97b359e57d
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-16 06:00:29 -0300

    fixing conflicts

--- src/pages/MyTineraries.js
+++ src/pages/MyTineraries.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Itineraries from '../components/Itineraries';
 import { useGetAllItinerariesUserQuery } from '../features/itinerariesAPI';
+import {Link as LinkRouter} from 'react-router-dom'
 
 const MyTineraries = () => {
     
     return (  
         <>
+            <div className='ButtonBackContainer'>
+                <LinkRouter to='/newitinerary' className='Button'>New Itinerary</LinkRouter>
+            </div>
             <Itineraries useGetAllItinerariesQuery={useGetAllItinerariesUserQuery}/>
         </>
     );
