58d2266adee0d5ddaaf33d0b0e4f53c305b82359
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-21 03:44:10 -0300

    Add button newItinerary logedin

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ButtonBack from '../components/ButtonBack';
 import DetailsCard from '../components/DetailsCard';
 import Itineraries from '../components/Itineraries';
+import {Link as LinkRouter, useParams} from 'react-router-dom'
 import { useGetAllItinerariesCityQuery } from '../features/itinerariesAPI';
 import '../styles/DetailsCard.css'
 
 const Details = () => {
+    const client = localStorage.getItem("client")
+ 
+    const {id} = useParams()
     return (  
         <div className='detailsContainer'>
+            <div className='Details-buttons'>
             <ButtonBack where={-1}/>
+            {client?
+             <div className='ButtonBackContainer'>
+                 <LinkRouter to={`/newitinerary/${id}`} className='Button'>New Itinerary</LinkRouter>
+             </div>
+            : null
+            }
+            </div>
             <DetailsCard/>
             <Itineraries useGetAllItinerariesQuery={useGetAllItinerariesCityQuery}/>
         </div>
