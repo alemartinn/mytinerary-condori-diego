2faab273ee3a957f9b3d6f6cd21024f7a758d993
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-23 08:33:07 -0300

    Rename comment component

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import Comments from './Comments';
+import Comments from './DisplayComments';
 import { useGetCommentsQuery } from '../features/commentAPI';
 import Activities from './Activities';
 import '../styles/Itinerary.css';
