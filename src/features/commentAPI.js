5573b4ed226c5bfac43a651947bd7ee73ab1c7d0
diego-nahuel <110680323+diego-nahuel@users.noreply.github.com>
2022-09-23 08:33:29 -0300

    add editComment endpoint

--- src/features/commentAPI.js
+++ src/features/commentAPI.js
@@ -15,16 +15,26 @@ const commentsAPI =  createApi({
             query:(id, comment) => ({
                 url:`/comments/${id}`,
                 method: 'PATCH',
-                body: comment
+                body: comment,
+                headers: {Authorization: `Bearer ${localStorage.getItem("token")}`}
             })
         }),
         deleteComment: builder.mutation({
             query: (id) => ({
                 url: `/comments/${id}`,
                 method: 'DELETE',
+                headers: {Authorization: `Bearer ${localStorage.getItem("token")}`}
+            })
+        }),
+        createComment: builder.mutation({
+            query:(newComment) => ({
+                url:`/comments`,
+                method: 'POST',
+                body: newComment,
+                headers: {Authorization: `Bearer ${localStorage.getItem("token")}`}
             })
         })
     })
 })
 export default commentsAPI
-export const {useGetCommentsQuery, useEditCommentMutation, useDeleteCommentMutation} = commentsAPI
+export const {useGetCommentsQuery, useEditCommentMutation, useDeleteCommentMutation, useCreateCommentMutation} = commentsAPI
