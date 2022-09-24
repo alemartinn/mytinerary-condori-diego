17d6ff8234cdd9a527b74a566d6337cc5e1c3804
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-24 15:02:52 -0300

    added userSlice redux for details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -4,17 +4,20 @@ import DetailsCard from '../components/DetailsCard';
 import Itineraries from '../components/Itineraries';
 import {Link as LinkRouter, useParams} from 'react-router-dom'
 import { useGetAllItinerariesCityQuery } from '../features/itinerariesAPI';
+import { useSelector } from 'react-redux';
 import '../styles/DetailsCard.css'
 
 const Details = () => {
-    const client = localStorage.getItem("client")
- 
+    
     const {id} = useParams()
+    const userRedux = useSelector(state => state.user.u);
+ 
     return (  
         <div className='detailsContainer'>
             <div className='Details-buttons'>
             <ButtonBack where={-1}/>
-            {client?
+            {userRedux 
+            ?
              <div className='ButtonBackContainer'>
                  <LinkRouter to={`/newitinerary/${id}`} className='Button'>New Itinerary</LinkRouter>
              </div>
