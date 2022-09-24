c7c8f122a77660686c69b46f0816d00cb6ba1124
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-24 15:07:30 -0300

    added userSlice redux

--- src/components/FormNewItinerary.js
+++ src/components/FormNewItinerary.js
@@ -4,22 +4,25 @@ import { useAddNewItineraryMutation } from '../features/itinerariesAPI'
 import '../styles/FormNewItinerary.css'
 import InputMod from './InputMod'
 import Alert from './Alert'
+import { useSelector } from 'react-redux';
 
 const FormNewItinerary = (props) => {
-    const client = localStorage.getItem("client")
-    const userLocal = JSON.parse(client)
-    const userId = userLocal.id
+    
+    const userRedux = useSelector(state => state.user.u);
+    const [newItinerary] = useAddNewItineraryMutation();
+    const Navigate = useNavigate();
+    
     const [itinerary, setItinerary] = useState({
-        name: "", user: userId, city: props.idCity, price: "", likes: [],
+        name: "", user: userRedux.id, city: props.idCity, price: "", likes: [],
         tags:[], duration: ""
-    })
+    });
 
     const modelForm = [
         {name: 'name', type: 'text'},
         {name: 'price', type: 'number'},
         {name: 'tags', type: 'text'},
         {name: 'duration', type: 'number'}
-    ]
+    ];
 
     const viewForm = (elem, index) => (
         <div className='form__group field' key={index}>
@@ -32,7 +35,7 @@ const FormNewItinerary = (props) => {
             />
             <label className='form__label' htmlFor={elem.name}>{elem.name}</label>
         </div>
-    )
+    );
 
     const handleChange = e => {
         if(e.target.name === "tags"){
@@ -44,10 +47,7 @@ const FormNewItinerary = (props) => {
                 ...itinerary, [e.target.name]: e.target.value
             })
         }
-    }
-
-    const [newItinerary] = useAddNewItineraryMutation()
-    const Navigate = useNavigate()
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -55,28 +55,23 @@ const FormNewItinerary = (props) => {
         if(error){
             Alert("error",error.data.message)
         } else {
-            console.log(data);
-            // await Swal.fire({
-            //     icon: 'success',
-            //     title: `Created Itinerary!`,
-            //     text: `You can see it in page`
-            // })
             Alert("success",data.message)
             Navigate(-1)
         }
     }
-  return (
-    <div className='NewItyn-container'>
-        <form className='NewItin-form' onSubmit={handleSubmit}>
-        <h2 className='Title-form'>New Itinerary</h2>
-        <div className='Inputs-form'>{modelForm.map(viewForm)}</div>
-        <button className="icon-btn add-btn" type='submit'>
-            <div className="add-icon"></div>
-            <div className="btn-txt">Add</div>
-        </button>
-        </form>
-    </div>
-  )
+        
+    return (
+        <div className='NewItyn-container'>
+            <form className='NewItin-form' onSubmit={handleSubmit}>
+            <h2 className='Title-form'>New Itinerary</h2>
+            <div className='Inputs-form'>{modelForm.map(viewForm)}</div>
+            <button className="icon-btn add-btn" type='submit'>
+                <div className="add-icon"></div>
+                <div className="btn-txt">Add</div>
+            </button>
+            </form>
+        </div>
+    )
 }
 
 export default FormNewItinerary
