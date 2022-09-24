a89c19e085d152dabf5232153ebd8d17186e0b04
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-24 15:05:03 -0300

    added userSlice redux for navbar

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,32 +1,26 @@
 import React, { useEffect } from 'react';
 import { Link as LinkRouter } from 'react-router-dom';
-import '../../styles/Header/NavBar.css'
+import '../../styles/Header/NavBar.css';
+import { useSelector } from 'react-redux';
 
 const NavBar = ({showNavMenu, clickShowNavMenu}) => {
     
+    const userRedux = useSelector(state => state.user.u)
+
     useEffect(() => {
         let headerNav = document.getElementById('HeaderNav')
         showNavMenu ? headerNav.style.left='0%' : headerNav.style.left='-100%'
     },[showNavMenu])
 
-    const client = localStorage.getItem("client")
-    const userLocal = JSON.parse(client)
-    let roleLocal = ""
-    if(userLocal && userLocal.role === "admin") {
-        roleLocal = "admin"
-    } else {
-        roleLocal = "user"
-    }
-
     return (  
         <nav className='HeaderNav' id='HeaderNav'>
-            {client?
+            {userRedux ?
             <>
-                {(roleLocal === "admin")?
+                {(userRedux.role === "admin")?
                 <>
                     <LinkRouter to='/' onClick={clickShowNavMenu}> HOME </LinkRouter>
                     <LinkRouter to='cities' onClick={clickShowNavMenu}> CITIES </LinkRouter>          
-                    <LinkRouter to={`mytineraries/${userLocal.id}`} onClick={clickShowNavMenu}> MyTINERARY </LinkRouter>    
+                    <LinkRouter to={`mytineraries/${userRedux.id}`} onClick={clickShowNavMenu}> MyTINERARY </LinkRouter>    
                     <LinkRouter to='editcity' onClick={clickShowNavMenu}> EDIT CITY </LinkRouter>
                     <LinkRouter to='newcities' onClick={clickShowNavMenu}> NEW CITY </LinkRouter>
                 </>
@@ -34,7 +28,7 @@ const NavBar = ({showNavMenu, clickShowNavMenu}) => {
                 <>
                     <LinkRouter to='/' onClick={clickShowNavMenu}> HOME </LinkRouter>
                     <LinkRouter to='/cities' onClick={clickShowNavMenu}> CITIES </LinkRouter>
-                    <LinkRouter to={`mytineraries/${userLocal.id}`} onClick={clickShowNavMenu}> MyTINERARY </LinkRouter> 
+                    <LinkRouter to={`mytineraries/${userRedux.id}`} onClick={clickShowNavMenu}> MyTINERARY </LinkRouter> 
                 </>
                 }
             </>
