9a2b04f6a7490e7d161e8a75f5b0d7356eca4e99
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-09-24 15:04:54 -0300

    added userSlice redux for user menu

--- src/components/Header/UserMenu.js
+++ src/components/Header/UserMenu.js
@@ -3,63 +3,61 @@ import { Link as LinkRouter } from 'react-router-dom';
 import { useSignOutMutation } from '../../features/authAPi';
 import '../../styles/Header/UserMenu.css';
 import Swal from 'sweetalert2';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeUser } from '../../features/userSlice';
+
 
 const UserMenu = ({showUserMenu, clickShowUserMenu}) => {
     
+    const dispatch = useDispatch();
+    const userRedux = useSelector(state => state.user.u);
+    const [signOut] = useSignOutMutation();
+
     useEffect(() => {
         let headerUserMenu = document.getElementById('HeaderUserMenu')
         showUserMenu ? headerUserMenu.style.right='0' : headerUserMenu.style.right='-200px'
-    },[showUserMenu])
+    },[showUserMenu]);
 
-    const client = localStorage.getItem("client")
-    const userLocal = JSON.parse(client)
-    let roleLocal = ""
-
-    if(userLocal && userLocal.role === "admin") {
-        roleLocal = "admin"
-    } else {
-        roleLocal = "user"
-    }
-
-    let name = userLocal?.name
-
-    const [signOut] = useSignOutMutation()
     const signOutUser = async (e) => {
-        let {error} =await signOut({email :userLocal.email})
+
+        let {error} =await signOut({email : userRedux.email});
         
         if(error){
-            console.log(error)
+            console.log(error);
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: `You couldn't sign out correctly, please try it again!`
-              })
+              });
         } else {
+            dispatch(removeUser(null))
             Swal.fire({
                 icon: 'success',
-                title: `See you soon ${(JSON.parse(localStorage.getItem('client'))).name} !`,
+                title: `See you soon ${userRedux.name} !`,
                 text: `You logged out succesfully.`
-            })
+            });
         }
         clickShowUserMenu()
         localStorage.removeItem("client")
+        localStorage.removeItem("token")
         // Navigate("/")
     }
 
     return (  
         <nav className='HeaderUserMenu' id='HeaderUserMenu'>
-            {client?
+            {userRedux 
+            ?
             <>
-                {(roleLocal === "admin")?
+                {(userRedux.role === "admin")?
                 <>
-                    <LinkRouter to={`/profile/${userLocal.id}`} onClick={clickShowUserMenu}><b>{name}</b></LinkRouter>
+                    <LinkRouter to={`/profile/${userRedux.id}`} onClick={clickShowUserMenu}><b>{userRedux.name}</b></LinkRouter>
                     <LinkRouter to='auth/signup' onClick={clickShowUserMenu}> New Admin </LinkRouter>
                     <LinkRouter to='/' onClick={signOutUser}> Sign Out </LinkRouter>
                 </>
                 :
                 <>
-                    <LinkRouter to={`/profile/${userLocal.id}`} onClick={clickShowUserMenu}><b>{name}</b></LinkRouter>
-                    <LinkRouter to={`mytineraries/${userLocal.id}`} onClick={clickShowUserMenu}> MyTinerary </LinkRouter>
+                    <LinkRouter to={`/profile/${userRedux.id}`} onClick={clickShowUserMenu}><b>{userRedux.name}</b></LinkRouter>
+                    <LinkRouter to={`mytineraries/${userRedux.id}`} onClick={clickShowUserMenu}> MyTinerary </LinkRouter>
                     <LinkRouter to='/' onClick={signOutUser}> Sign Out </LinkRouter>
                 </>
                 }
